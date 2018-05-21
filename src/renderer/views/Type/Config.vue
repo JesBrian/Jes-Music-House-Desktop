af047647a667188d2075a6f8ca522b421f279527
JesBrian <jesbrian666@gmail.com>
2018-05-21 21:49:50 +0800

    配置&设置界面 - [ 构建ing ]

--- src/renderer/views/Type/Config.vue
+++ src/renderer/views/Type/Config.vue
@@ -1,17 +1,117 @@
 <template>
-	<div style="padding:63px 3% 18px 208px; position:relative;">
-    <div style="width:76.5%; height:53px; top:53px; left:245px; margin:0 auto 28px; padding:0 38px; position:fixed; box-sizing:border-box; background:#181818; box-shadow:0 3px 3px -3px #20dbfc; line-height:68px; text-shadow:1px 1px 0.5px #000;">
+	<div style="height:100%; padding:63px 3% 18px 208px; position:relative; box-sizing:border-box;">
+
+    <div style="width:76.5%; height:53px; top:53px; left:245px; margin:0 auto 28px; padding:0 38px; position:fixed; z-index:9; box-sizing:border-box; background:#181818; box-shadow:0 3px 3px -3px #20dbfc; line-height:68px; text-shadow:1px 1px 0.5px #000;">
       <span style="font-size:23px; font-weight:700; color:#CCC;">系统设置</span>
     </div>
 
-    <div style="width:100%;">
+    <div style="width:148px; top:138px; left:248px; padding-right:18px; position:fixed; text-align:right;">
 
-      <div style="width:168px; height:338px; top:138px; left:248px; position:fixed; background:lightgreen"></div>
+      <ul>
+        <li>
+          <div class="config-nav-title">
+          <p>账号</p>
+          </div>
+        </li>
+        <li>
+          <div class="config-nav-title">
+          <p>常规</p>
+          </div>
+        </li>
+        <li>
+          <div class="config-nav-title">
+          <p>播放</p>
+          </div>
+        </li>
+        <li>
+          <div class="config-nav-title">
+          <p>消息与隐私</p>
+          </div>
+        </li>
+        <li>
+          <div class="config-nav-title">
+          <p>快捷键</p>
+          </div>
+        </li>
+        <li>
+          <div class="config-nav-title active">
+          <p>下载设置</p>
+          </div>
+        </li>
+        <li>
+          <div class="config-nav-title">
+          <p>歌词</p>
+          </div>
+        </li>
+        <li>
+          <div class="config-nav-title">
+          <p>工具</p>
+          </div>
+        </li>
+        <li>
+          <div class="config-nav-title">
+          <p>关于 Music House</p>
+          </div>
+        </li>
+      </ul>
 
-      <div>
-        this is config content
+      <div class="box-show" style="width:5px; height:90%; top:14px; right:0; float:right; position:absolute; background-image: linear-gradient(#2af1fc, #1a9ee6); cursor:pointer;">
+        <div style="width:15px; height:15px; top:62.7%; right:-6px; margin-top:-8.6px; position:absolute; border-radius:50%; background:url(../../../../../static/images/default/slide-pointer.png) no-repeat; background-size:100% 100%;"></div>
+        <div class="super-btn-out" style="width:10px; height:10px; top:-5px; right:-3.5px; position:absolute; border-radius:50%;"></div>
+        <div class="super-btn-out" style="width:10px; height:10px; bottom:-5px; right:-3.5px; position:absolute; border-radius:50%;"></div>
       </div>
     </div>
+
+    <div style="width:68%; margin:8px 38px 18px; position:absolute; box-sizing:border-box; overflow:hidden;">
+      <ul>
+        <li class="box-shadow">
+          <div style="height:120px; margin-bottom:12px;">
+            <p class="config-item-title">账号</p>
+          </div>
+        </li>
+        <li class="box-shadow">
+          <div style="height:120px; margin-bottom:12px;">
+            <p class="config-item-title">常规</p>
+          </div>
+        </li>
+        <li class="box-shadow">
+          <div style="height:120px; margin-bottom:12px;">
+            <p class="config-item-title">播放</p>
+          </div>
+        </li>
+        <li class="box-shadow">
+          <div style="height:120px; margin-bottom:12px;">
+            <p class="config-item-title">消息与隐私</p>
+          </div>
+        </li>
+        <li class="box-shadow">
+          <div style="height:120px; margin-bottom:12px;">
+            <p class="config-item-title">快捷键</p>
+          </div>
+        </li>
+        <li class="box-shadow">
+          <div style="height:120px; margin-bottom:12px;">
+            <p class="config-item-title">下载设置</p>
+          </div>
+        </li>
+        <li class="box-shadow">
+          <div style="height:120px; margin-bottom:12px;">
+            <p class="config-item-title">歌词</p>
+          </div>
+        </li>
+        <li class="box-shadow">
+          <div style="height:120px; margin-bottom:12px;">
+            <p class="config-item-title">工具</p>
+          </div>
+        </li>
+        <li class="box-shadow">
+          <div style="height:120px; margin-bottom:12px;">
+            <p class="config-item-title">关于 Music House</p>
+          </div>
+        </li>
+      </ul>
+    </div>
+
 	</div>
 </template>
 
@@ -24,5 +124,18 @@ export default {
 </script>
 
 <style scoped>
+  .config-nav-title {
+    padding:8px 0;
+  }
+  .config-nav-title > p {
+    cursor:pointer;
+  }
+  .config-nav-title.active > p {
+    color: #00d8ff;
+    text-shadow:2px 2px 2px #000;
+  }
 
+  .config-item-title {
+    line-height:2em; font-size:20px;
+  }
 </style>
--- src/renderer/views/Type/Setting.vue
+++ src/renderer/views/Type/Setting.vue
@@ -1,17 +1,53 @@
 <template>
-	<div style="padding:0 3%;">
+	<div style="width:100%; height:100%; padding:0 3%; box-sizing:border-box; background:#333;">
     <div style="width:100%; height:53px; margin:0 auto 28px; padding:0 38px; box-sizing:border-box; box-shadow:0 3px 3px -3px #20dbfc; line-height:68px; text-shadow:1px 1px 0.5px #000;">
       <span style="font-size:23px; font-weight:700; color:#CCC;">编辑个人信息</span>
     </div>
 
     <!-- 基本信息 -->
-    <div style="width:62%; height:100%; display:inline-block;">
+    <div style="width:62%; display:inline-block;">
 
+      <table style="width:88%; margin:0 auto; background:lightseagreen;">
+        <tr>
+          <td style="padding-bottom:18px; text-align:right;">昵称</td>
+          <td>
+            <input type="text" name="name" class="glass-bg box-show"/>
+          </td>
+        </tr>
+        <tr>
+          <td style="padding-bottom:18px; text-align:right;">介绍</td>
+          <td>
+            <textarea class="glass-bg box-show"></textarea>
+          </td>
+        </tr>
+        <tr>
+          <td style="padding-bottom:18px; text-align:right;">性别</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td style="padding-bottom:18px; text-align:right;">生日</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td style="padding-bottom:18px; text-align:right;">地区</td>
+          <td></td>
+        </tr>
+      </table>
+
+      <!-- 保存按钮区域 -->
+      <div style="margin-top:28px; text-align:center;">
+        <div class="super-btn-out" style="width:108px; height:32px; margin:0 18px;">
+          <span class="super-btn-in mh-if makesure" style="width:98px; height:22px; line-height:23px;"> 确认保存</span>
+        </div>
+        <div class="super-btn-out" style="width:108px; height:32px; margin:0 18px;">
+          <span class="super-btn-in mh-if cancel" style="width:98px; height:22px; line-height:23px;"> 取消返回</span>
+        </div>
+      </div>
     </div>
 
     <!-- 修改头像 -->
     <div style="width:38%; float:right; text-align:center;">
-      <img src="http://p2.music.126.net/kaISxJU3yP0Qvw6H_vUyAQ==/18984167765401316.jpg?param=80y80" class="cube-bg box-show" style="width:138px; height:138px; margin:0 auto; padding:5px; display:block;"/>
+      <img src="http://p2.music.126.net/kaISxJU3yP0Qvw6H_vUyAQ==/18984167765401316.jpg?param=80y80" class="glass-bg box-show" style="width:138px; height:138px; margin:0 auto; padding:5px; display:block;"/>
       <div class="super-btn-out" style="width:128px; height:32px; margin-top:12px;">
         <span class="super-btn-in mh-if datum" style="width:118px; height:22px; line-height:22px;"> 修改头像</span>
       </div>
