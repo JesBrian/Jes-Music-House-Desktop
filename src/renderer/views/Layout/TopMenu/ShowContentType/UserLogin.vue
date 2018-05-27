dddb7e4648e94058b66e5f282cc091a9165a622a
JesBrian <jesbrian666@gmail.com>
2018-05-27 15:06:44 +0800

    登陆 - [ update 登陆组件 ]

--- src/renderer/views/Layout/TopMenu/ShowContentType/UserLogin.vue
+++ src/renderer/views/Layout/TopMenu/ShowContentType/UserLogin.vue
@@ -6,9 +6,53 @@
     </div>
 
     <div style=" padding:0 1px;">
-      <div class="box-shadow" style="height:88px;"></div>
 
-      <div class="box-shadow" style="margin-top:8px; text-align:center;">
+      <!-- 登录/注册 -->
+      <div class="box-shadow" style="height:188px;">
+        <!-- 选择按钮 -->
+        <div v-if="formType === 'chooseContent'" style="padding-top:38px;">
+          <div @click="changeFormType('loginContent')" class="super-btn-out" style="width:72%; height:34px; margin:0 auto 18px; display:block; border-radius:17px;">
+            <span class="super-btn-in" style="width:94%; height:26px; top:48%; border-radius:13px; line-height:28px; font-size:18px;">手机号登录</span>
+          </div>
+          <div @click="changeFormType('registerContent')" class="super-btn-out" style="width:72%; height:34px; margin:0 auto 18px; display:block; border-radius:17px;">
+            <span class="super-btn-in" style="width:94%; height:26px; top:48%; border-radius:13px; line-height:28px; font-size:18px;">注册</span>
+          </div>
+        </div>
+
+         <!-- 登录表单 -->
+        <div v-else-if="formType === 'loginContent'">
+
+          <div @click="changeFormType('chooseContent')" class="super-btn-out" style="width:56px; height:23px; margin:3px;">
+            <span class="super-btn-in" style="width:50px; height:18px; line-height:20px; font-size:13px;">< 返回</span>
+          </div>
+
+          <div>
+          </div>
+
+          <div class="super-btn-out" style="width:72%; height:34px; margin:14px auto 0; display:block; border-radius:17px;">
+            <span class="super-btn-in" style="width:94%; height:26px; top:48%; border-radius:13px; line-height:28px; font-size:18px;">登录</span>
+          </div>
+        </div>
+
+        <!-- 注册表单 -->
+        <div v-else-if="formType === 'registerContent'">
+
+          <div @click="changeFormType('chooseContent')" class="super-btn-out" style="width:56px; height:23px; margin:3px;">
+            <span class="super-btn-in" style="width:50px; height:18px; line-height:20px; font-size:13px;">< 返回</span>
+          </div>
+
+          <div class="super-btn-out" style="width:72%; height:34px; margin:14px auto 0; display:block; border-radius:17px;">
+            <span class="super-btn-in" style="width:94%; height:26px; top:48%; border-radius:13px; line-height:28px; font-size:18px;">注册</span>
+          </div>
+        </div>
+
+      </div>
+
+      <!-- 第三方登录 -->
+      <div class="box-shadow" style="margin:15px 0 13px; position:relative; text-align:center;">
+
+        <span style="top:-22px; left:50%; padding:0 8px; position:absolute; transform:translateX(-50%); z-index:9; background:#111;">其他登录方式</span>
+
         <div style="height:53px; padding:0 6px; margin-bottom:8px;">
           <div style="width:33%; height:100%; float:left;">
             <div class="super-btn-out" style="width:38px; height:38px; margin-top:7px;">
@@ -54,7 +98,17 @@
 export default {
   name: 'UserLogin',
 
+  data () {
+    return {
+      formType: 'chooseContent'
+    }
+  },
+
   methods: {
+    changeFormType (type = 'chooseContent') {
+      this.formType = type
+    },
+
     oauthLogin () {
       this.$store.commit('CHANGE_MODAL_TYPE', 'OauthLogin')
     }
