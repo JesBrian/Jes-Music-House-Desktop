8532d8ee5fb21222d8768cd4dc11b9167071ca5e
JesBrian <jesbrian666@gmail.com>
2018-06-03 16:50:18 +0800

    主页 - [ update 歌手部分 again ]

--- src/renderer/components/extends/index-content/news-music/IndexNewsMusic.vue
+++ src/renderer/components/extends/index-content/news-music/IndexNewsMusic.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div style="height:688px;">
     this is index IndexNewsMusic
   </div>
 </template>
--- src/renderer/components/extends/index-content/singer/IndexSinger.vue
+++ src/renderer/components/extends/index-content/singer/IndexSinger.vue
@@ -1,8 +1,29 @@
 <template>
   <div>
-
-    <div style="width:92%; margin:0 auto 28px; padding-bottom:28px; box-shadow:0 2px 3px -3px #FFF;">
-      <div style="width:100%; height:88px; background:lightskyblue"></div>
+    <div style="width:92%; margin:-16px auto 28px; padding:0 0 10px 16px; box-sizing:border-box; box-shadow:0 2px 3px -3px #FFF;">
+      <div style="width:100%; line-height:2.38em; font-size:14px;">
+        <p>
+          <span style="margin-right:8px;">语种:</span>
+          <span class="cube-bg box-show" style="margin:0 3px; padding:4px 6px 4px 7px; box-sizing:border-box; color:#20DBFC; text-shadow:2px 2px 2px #000;">全部</span>
+          <span class="" style="margin:0 3px; padding:4px 6px 4px 7px; box-sizing:border-box; color:#AAA; text-shadow:2px 2px 2px #000;">华语</span>
+          <span class="" style="margin:0 3px; padding:4px 6px 4px 7px; box-sizing:border-box; color:#AAA; text-shadow:2px 2px 2px #000;">欧美</span>
+          <span class="" style="margin:0 3px; padding:4px 6px 4px 7px; box-sizing:border-box; color:#AAA; text-shadow:2px 2px 2px #000;">日本</span>
+          <span class="" style="margin:0 3px; padding:4px 6px 4px 7px; box-sizing:border-box; color:#AAA; text-shadow:2px 2px 2px #000;">韩国</span>
+          <span class="" style="margin:0 3px; padding:4px 6px 4px 7px; box-sizing:border-box; color:#AAA; text-shadow:2px 2px 2px #000;">其他</span>
+        </p>
+        <p>
+          <span style="margin-right:8px;">分类:</span>
+          <span class="cube-bg box-show" style="margin:0 3px; padding:4px 6px 4px 7px; box-sizing:border-box; color:#20DBFC; text-shadow:2px 2px 2px #000;">全部</span>
+          <span class="" style="margin:0 3px; padding:4px 6px 4px 7px; box-sizing:border-box; color:#AAA; text-shadow:2px 2px 2px #000;">男歌手</span>
+          <span class="" style="margin:0 3px; padding:4px 6px 4px 7px; box-sizing:border-box; color:#AAA; text-shadow:2px 2px 2px #000;">女歌手</span>
+          <span class="" style="margin:0 3px; padding:4px 6px 4px 7px; box-sizing:border-box; color:#AAA; text-shadow:2px 2px 2px #000;">乐队组合</span>
+        </p>
+        <p>
+          <span style="margin-right:8px;">筛选:</span>
+          <span class="cube-bg box-show" style="margin:0 3px; padding:4px 6px 4px 7px; box-sizing:border-box; color:#00d8ff; text-shadow:2px 2px 2px #000;">热门</span>
+          <span v-for="n in 26" class="" style="margin:0 3px; padding:4px 6px 4px 7px; box-sizing:border-box; color:#AAA; text-shadow:2px 2px 2px #000;">{{ String.fromCharCode(64 + n) }}</span>
+        </p>
+      </div>
     </div>
 
     <div style="width:92%; margin:0 auto; text-align:center;">
