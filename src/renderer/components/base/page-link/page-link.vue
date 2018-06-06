bf9082604c695a041a8bde6d7da439593f38f698
JesBrian <jesbrian666@gmail.com>
2018-06-06 21:48:14 +0800

    路由跳转 - [ 尝试拆分跳转组件 ]

--- src/renderer/components/base/page-link/page-link.vue
+++ src/renderer/components/base/page-link/page-link.vue
@@ -1,10 +1,24 @@
 <template>
-  
+  <div @click="changePage" style="margin:0; padding:0; display:inline-block; cursor:pointer;">
+    <slot />
+  </div>
 </template>
 
 <script>
+import { changePage } from '../../../assets/js/commom.js'
+
 export default {
-  name: 'page-link'
+  name: 'page-link',
+
+  props: {
+    url: String
+  },
+
+  methods: {
+    changePage () {
+      changePage(this.url, this)
+    }
+  }
 }
 </script>
 
