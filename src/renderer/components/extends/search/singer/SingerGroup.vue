f052320990dbf14ac08a0df68d4521ef620aa6e8
jesbrian <jesbrian666@gmail.com>
2018-06-30 11:48:25 +0800

    Linux - [ fix upper bug ]

--- src/renderer/components/extends/search/singer/SingerGroup.vue
+++ src/renderer/components/extends/search/singer/SingerGroup.vue
@@ -1,13 +1,13 @@
 <template>
   <ul>
-    <li v-for="n in 18" style="width:46%; margin:25px 0 0 3%; display:inline-block;">
+    <li v-for="n in 18" :key="n + 99" style="width:46%; margin:25px 0 0 3%; display:inline-block;">
       <singer-item />
     </li>
   </ul>
 </template>
 
 <script>
-  import SingerItem from './Singeritem.vue'
+  import SingerItem from './SingerItem.vue'
 
   export default {
     name: 'SingerGroup',
