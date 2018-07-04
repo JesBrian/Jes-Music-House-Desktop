916a9b9cd6029191612d5ad15ad9af94b8b404be
JesBrian <jesbrian666@gmail.com>
2018-07-04 21:09:29 +0800

    上传图片 - [ 鬼知道我经历了什么 ]

--- src/renderer/views/pages/SettingPage.vue
+++ src/renderer/views/pages/SettingPage.vue
@@ -53,19 +53,74 @@
 
     <!-- 修改头像 -->
     <div style="width:38%; float:right; text-align:center;">
-      <img src="http://p2.music.126.net/kaISxJU3yP0Qvw6H_vUyAQ==/18984167765401316.jpg?param=80y80" class="glass-bg box-show" style="width:138px; height:138px; margin:0 auto; padding:5px; display:block;"/>
+      <img :src="imgDataUrl" class="glass-bg box-show" style="width:138px; height:138px; margin:0 auto; padding:5px; display:block;"/>
       <div class="super-btn-out" style="width:128px; height:32px; margin-top:12px;">
         <span class="super-btn-in mh-if datum" style="width:118px; height:22px; line-height:22px;">&nbsp;&nbsp;修改头像</span>
       </div>
     </div>
+
+    <my-upload field="img" @crop-success="cropSuccess" @crop-upload-success="cropUploadSuccess" @crop-upload-fail="cropUploadFail" v-model="show" :width="300" :height="300" url="/upload" :params="params" :headers="headers" img-format="png"></my-upload>
 	</div>
 </template>
 
 <script>
+  import MyUpload from 'vue-image-crop-upload'
+
   export default {
     name: 'SettingPage',
 
-    components: {}
+    components: {
+      MyUpload
+    },
+
+    data () {
+      return {
+        show: true,
+        params: {
+          token: '123456798',
+          name: 'avatar'
+        },
+        headers: {
+          smail: '*_~'
+        },
+        imgDataUrl: 'http://p2.music.126.net/kaISxJU3yP0Qvw6H_vUyAQ==/18984167765401316.jpg?param=80y80'
+      }
+    },
+
+    methods: {
+      toggleShow () {
+        this.show = !this.show
+      },
+      /**
+       * crop success
+       * [param] imgDataUrl
+       * [param] field
+       */
+      cropSuccess (imgDataUrl, field) {
+        console.log('-------- crop success --------')
+        this.imgDataUrl = imgDataUrl
+      },
+      /**
+       * upload success
+       * [param] jsonData   服务器返回数据，已进行json转码
+       * [param] field
+       */
+      cropUploadSuccess (jsonData, field) {
+        console.log('-------- upload success --------')
+        console.log(jsonData)
+        console.log('field: ' + field)
+      },
+      /**
+       * upload fail
+       * [param] status    server api return error status, like 500
+       * [param] field
+       */
+      cropUploadFail (status, field) {
+        console.log('-------- upload fail --------')
+        console.log(status)
+        console.log('field: ' + field)
+      }
+    }
   }
 </script>
 
