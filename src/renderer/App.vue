9def9643f2ab1351aef5370ac7755a652ae4dd69
JesBrian <JesBrian666@gmail.com>
2018-08-03 19:52:42 +0800

    函数 - [ 优化 ]

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -11,30 +11,43 @@
     created () {
       // vuex + localForage
 
-      // 客户端配置信息
-      this.$localForage.getItem('config', (result, value) => {
-        if (value) {
-        }
-      })
+      this.clientConfig()
+      this.userConfig()
+    },
 
-      // 登录用户信息
-      this.$localForage.getItem('user', (result, value) => {
-        if (value) { // 如果用户已经登陆了
-          // 检验 localForage 所存的用户信息是否正确
-          this.$http.post('verifyUserLogin', {
-            userId: value.id,
-            username: value.username
-          }).then((response) => {
-            if (response.data.state === '200') {
-              this.$store.commit('SAVE_LOGIN_USER_INFO', value)
-            } else {
-              this.$localForage.setItem('user', null)
-            }
-          }).catch((error) => {
-            console.log(error)
-          })
-        }
-      })
+    methods: {
+      /**
+       * 客户端配置信息
+       */
+      clientConfig () {
+        this.$localForage.getItem('config', (result, value) => {
+          if (value) {
+          }
+        })
+      },
+
+      /**
+       * 登录用户信息
+       */
+      userConfig () {
+        this.$localForage.getItem('user', (result, value) => {
+          if (value) { // 如果用户已经登陆了
+            // 检验 localForage 所存的用户信息是否正确
+            this.$http.post('verifyUserLogin', {
+              userId: value.id,
+              username: value.username
+            }).then((response) => {
+              if (response.data.state === '200') {
+                this.$store.commit('SAVE_LOGIN_USER_INFO', value)
+              } else {
+                this.$localForage.setItem('user', null)
+              }
+            }).catch((error) => {
+              console.log(error)
+            })
+          }
+        })
+      }
     }
   }
 </script>
