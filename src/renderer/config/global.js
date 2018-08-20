76847ceaf9ff358e48ce5d41af770696b7900a7c
JesBrian <JesBrian666@gmail.com>
2018-08-20 22:17:19 +0800

    mac 配置 - [ 适配 ]

--- src/renderer/config/global.js
+++ src/renderer/config/global.js
@@ -1,7 +1,7 @@
 
 export const Global = {
   DEV_API_URL: 'http://local.musichouse.cn/api/',
-  DEV_RESOURCE_URL: 'http://local.jesbrian.cn/Jes-MusicHouse/MusicHouse-Backstage/resource/',
+  DEV_RESOURCE_URL: 'http://music.jesbrian.local/resource/',
   PROD_API_URL: '',
   PROD_RESOURCE_URL: ''
 }
